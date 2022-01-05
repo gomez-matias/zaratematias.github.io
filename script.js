1a86f8acb5f2d79159c26553dfa7622582fc4dde
matiaszarate <91911234+zaratematias@users.noreply.github.com>
2022-01-05 11:12:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ const btnMenu = document.getElementById('btnMenu');
 const navlinks = document.querySelectorAll('.navlink');
 const question = document.querySelectorAll('.pregunta');
 
+
+// alert(sections.length);
+// Boton Menu
 btnMenu.addEventListener('click', ()=> {
   btnMenu.classList.toggle('open');
   header.classList.toggle('open-menu');
 });
 
+// Los botones del menu cierran el menu al ser precionados
 navlinks.forEach((element =>{
   element.addEventListener('click', ()=>{
     header.classList.toggle('open-menu');
@@ -16,6 +20,7 @@ navlinks.forEach((element =>{
   })
 }))
 
+// Acordeon Dudas
 question.forEach((element)=>{
     element.addEventListener('click', (e) => {
       if(!(e.currentTarget.classList.contains('open'))){
@@ -28,4 +33,28 @@ question.forEach((element)=>{
         e.currentTarget.classList.remove('open');
       }
     })
+});
+
+
+// Intersection Observer
+const sections = document.querySelectorAll('section')
+const showElement = (entradas, observador) => {
+  entradas.forEach((entrada)=>{
+    if(entrada.isIntersecting){
+      entrada.target.classList.add('visible');
+    }
+  })
+  // sections.forEach((element)=>{
+    
+  // })
+};
+const observer = new IntersectionObserver(showElement,{
+  root: null,
+  rootMargin: '0px 0% 0% 0%'
+});
+sections.forEach((element)=>{
+  observer.observe(element);
 })
+// End Intersection Observer
+
+
